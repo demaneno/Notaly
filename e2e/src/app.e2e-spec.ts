ff1d8b2ce0e640667040b217c1f8e1d353d5afd0
demaneno <2nis.maneno@gmail.com>
2020-05-10 17:18:42 +0200

    Update gh-pages

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('notes-frontend app is running!');
+    expect(page.getTitleText()).toEqual('notaly app is running!');
   });
 
   afterEach(async () => {
